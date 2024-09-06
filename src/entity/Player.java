ead1d82e4394dc934c0dbc9b4ed470f41baaf6ac
Ali Shibli <alishibli372@gmail.com>
2024-09-06 10:32:44 +0300

    Refactor Player class to support idle animations

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,13 +1,8 @@
 package entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.nio.Buffer;
-
 import javax.imageio.ImageIO;
-
 import main.GamePanel;
 import main.KeyHandler;
 
@@ -16,16 +11,26 @@ public class Player extends Entity {
     GamePanel gamePanel;
     KeyHandler keyHandler;
 
+    public final int screenX;
+    public final int screenY;
+
+    BufferedImage[] idleDown;
+    BufferedImage[] idleRight;
+    BufferedImage[] idleLeft;
+    BufferedImage[] idleUp;
+
     public Player(GamePanel gamePanel, KeyHandler keyHandler) {
         this.gamePanel = gamePanel;
         this.keyHandler = keyHandler;
+        screenX = gamePanel.screenWidth / 2 - gamePanel.tileSize / 2;
+        screenY = gamePanel.screenHeight / 2 - gamePanel.tileSize / 2;
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = gamePanel.tileSize * 17;
+        worldY = gamePanel.tileSize * 23;
         speed = 4;
         direction = "down";
     }
@@ -40,76 +45,114 @@ public class Player extends Entity {
             left2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png"));
             right1 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png"));
             right2 = ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png"));
+
+            // Load idle images
+            idleDown = new BufferedImage[6];
+            idleRight = new BufferedImage[6];
+            idleLeft = new BufferedImage[6];
+            idleUp = new BufferedImage[6];
+
+            for (int i = 0; i < 6; i++) {
+                idleDown[i] = ImageIO
+                        .read(getClass().getResourceAsStream("/player/idle/boy_idle_down_" + (i) + ".png"));
+                idleRight[i] = ImageIO
+                        .read(getClass().getResourceAsStream("/player/idle_right/boy_idle_right_" + (i) + ".png"));
+                idleLeft[i] = ImageIO
+                        .read(getClass().getResourceAsStream("/player/idle_left/boy_idle_left_" + (i) + ".png"));
+                idleUp[i] = ImageIO
+                        .read(getClass().getResourceAsStream("/player/idle_up/boy_idle_up_" + (i) + ".png"));
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public void update() {
+        boolean moving = false;
+
         if (keyHandler.up) {
             direction = "up";
-            y -= speed;
+            worldY -= speed;
+            moving = true;
         }
         if (keyHandler.down) {
-            y += speed;
             direction = "down";
+            worldY += speed;
+            moving = true;
         }
         if (keyHandler.left) {
-            x -= speed;
             direction = "left";
+            worldX -= speed;
+            moving = true;
         }
         if (keyHandler.right) {
-            x += speed;
             direction = "right";
+            worldX += speed;
+            moving = true;
         }
-        if (keyHandler.moving) {
+
+        if (moving) {
             spriteCounter++;
             if (spriteCounter > 10) {
                 spriteCounter = 0;
                 spriteNum = spriteNum == 1 ? 2 : 1;
             }
+        } else {
+            // Handle idle animation
+            spriteCounter++;
+            if (spriteCounter > 10) {
+                spriteCounter = 0;
+                spriteNum++;
+                if (spriteNum >= idleDown.length) {
+                    spriteNum = 0;
+                }
+            }
         }
-
     }
 
     public void draw(Graphics2D g) {
-
         BufferedImage img = null;
 
-        switch (direction) {
-            case "up":
-                if (spriteNum == 1) {
-                    img = up1;
-
-                } else if (spriteNum == 2) {
-                    img = up2;
-                }
-                break;
-            case "down":
-                if (spriteNum == 1) {
+        if (keyHandler.up || keyHandler.down || keyHandler.left || keyHandler.right) {
+            switch (direction) {
+                case "up":
+                    img = spriteNum == 1 ? up1 : up2;
+                    break;
+                case "down":
+                    img = spriteNum == 1 ? down1 : down2;
+                    break;
+                case "left":
+                    img = spriteNum == 1 ? left1 : left2;
+                    break;
+                case "right":
+                    img = spriteNum == 1 ? right1 : right2;
+                    break;
+                default:
                     img = down1;
-                } else if (spriteNum == 2) {
-                    img = down2;
-                }
-                break;
-            case "left":
-                if (spriteNum == 1) {
-                    img = left1;
-                } else if (spriteNum == 2) {
-                    img = left2;
-                }
-                break;
-            case "right":
-                if (spriteNum == 1) {
-                    img = right1;
-                } else if (spriteNum == 2) {
-                    img = right2;
-                }
-                break;
-            default:
-                img = down1;
-                break;
+                    break;
+            }
+        } else {
+            // Draw idle animation based on the last direction
+            switch (direction) {
+                case "down":
+                    img = idleDown[spriteNum];
+                    break;
+                case "right":
+                    img = idleRight[spriteNum];
+                    break;
+                case "left":
+                    img = idleLeft[spriteNum];
+                    break;
+                case "up":
+                    img = idleUp[spriteNum];
+                    break;
+                default:
+                    img = idleDown[spriteNum];
+                    break;
+            }
         }
-        g.drawImage(img, x, y, gamePanel.tileSize, gamePanel.tileSize, null);
+
+        g.drawImage(img, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
     }
 }
